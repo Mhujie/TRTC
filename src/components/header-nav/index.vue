244b2cc87c5b6da3b10b700229b3d6848b4fec66
Mhujie <1549551962@qq.com>
2022-02-28 17:37:10 +0800

    提交了完整trtc文件

---
+++ src/components/header-nav/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="header-nav">
+    <div class="header-nav-left">
+      <div class="header-nav-title">实时视频语音聊天</div>
+      <div class="header-nav-homepage" @click="gotoHomePage">首页</div>
+    </div>
+    <div class="header-nav-help">
+      <el-dropdown @command="handleCommand">
+        <span class="el-dropdown-link">
+          更多
+          <i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="command-detect">环境设备检测</el-dropdown-item>
+          <el-dropdown-item command="command-logout">注销</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import { setUserLoginInfo } from '../../utils'
+export default {
+  name: 'HeaderNav',
+  methods: {
+    handleCommand: function (command) {
+      if (command === 'command-detect') {
+        window.open('https://web.sdk.qcloud.com/trtc/webrtc/demo/detect/index.html', '__blank')
+        return
+      }
+
+      if (command === 'command-logout') {
+        this.$trtcCalling.logout()
+        this.$store.commit('userLogoutSuccess')
+        setUserLoginInfo({
+          token: '',
+          phoneNum: ''
+        })
+      }
+    },
+    gotoHomePage: function () {
+      if (this.$router.currentRoute.fullPath !== '/') {
+        this.$router.push('/')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header-nav {
+  background: -webkit-linear-gradient(left, rgb(255, 175, 2), skyblue, rgb(250, 213, 1));
+  height: 80px;
+  display: flex;
+  padding: 0 20%;
+  justify-content: space-around;
+  color: #ffffff;
+}
+.header-nav-left {
+  display: flex;
+  flex-direction: row;
+}
+.header-nav-title {
+  font-size: 22px;
+  display: flex;
+  align-items: center;
+}
+.header-nav-homepage {
+  margin-left: 50px;
+  font-size: 22px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.header-nav-help {
+  font-size: 22px;
+  display: flex;
+  align-items: center;
+}
+.el-dropdown-link {
+  color: #ffffff;
+  font-size: 22px;
+  cursor: pointer;
+}
+@media screen and (max-width: 767px) {
+  .header-nav {
+    padding: 0;
+  }
+  .header-nav-left {
+    justify-content: space-around;
+    width: 75%;
+  }
+  .header-nav-title,
+  .el-dropdown-link {
+    font-size: 18px;
+  }
+  .header-nav-homepage {
+    font-size: 18px;
+    margin-left: 10px;
+  }
+}
+</style>
